609f1bd73b9ad830deb5282c77bc7e6184406f49
jmbaik <famian@daum.net>
2024-06-21 03:54:24 +0900

    malltype 따른 color 변경

--- lib/presentation/pages/main/component/top_app_bar/default_app_bar.dart
+++ lib/presentation/pages/main/component/top_app_bar/default_app_bar.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/theme/custom/custom_theme.dart';
 import '../../cubit/bottom_nav_cubit.dart';
+import '../../cubit/mall_typ_cubit.dart';
 
 class DefaultAppBar extends StatelessWidget {
   const DefaultAppBar(this.bottomNav, {super.key});
@@ -8,21 +11,27 @@ class DefaultAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).colorScheme.primary,
-      padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-      child: AppBar(
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        title: Text(
-          bottomNav.toName,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-            color: Colors.white,
+    return BlocBuilder<MallTypeCubit, MallType>(
+      builder: (_, state) {
+        return Container(
+          color: state.isMarket
+              ? Theme.of(context).colorScheme.primary
+              : Theme.of(context).colorScheme.background,
+          padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            title: Text(
+              bottomNav.toName,
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    color: state.isMarket
+                        ? Theme.of(context).colorScheme.background
+                        : Theme.of(context).colorScheme.contentPrimary,
+                  ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
